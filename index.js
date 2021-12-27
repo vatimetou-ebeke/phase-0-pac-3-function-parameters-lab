13fbcc81520f54c13d2c3ec7ae33cbb59cd8768d
vatimetou ebeker <fatimaabcar2@gmail.com>
2021-12-27 18:12:49 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,24 @@
+ 
+ let name = "AKI";
+ let language=" Ember.js";
+ let name1="Gracie";
+ 
+ 
+ function introduction(name){
+    return(`Hi, my name is ${name}.`);
+ }
+
+
+
+
+
+function introductionWithLanguage(name,language){
+    return(`Hi, my name is ${name} and I am learning to program in ${language}.`);
+}
+
+function introductionWithLanguageOptional(name1, Optional="JavaScript"){
+    return(`Hi, my name is ${name1} and I am learning to program in ${Optional}.`);
+   
+
+    
+}
